26f3a1132e6a9cfebbccacca9e9e4b5dbc4c151f
Istvan Nemeth <nemethistvanius@gmail.com>
2021-04-21 20:13:49 +0200

    feat(controllers): add controllers package

---
+++ controllers/main_controller.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// StartupRouter creates instance of registers all the routes of the subroutes, supposed to be called in main func
+func StartupRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", welcomeHandler).Methods(http.MethodGet, http.MethodOptions)
+	return router
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to DomafocApp!"))
+}
--- main.go
+++ main.go
@@ -4,17 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/IstvanN/domafocapp-backend/controllers"
 	"github.com/IstvanN/domafocapp-backend/database"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to DomafocApp!"))
-	})
+	router := controllers.StartupRouter()
 
 	database.StartupPostgres()
 	log.Println("domafocapp-backend is up and running on port :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
