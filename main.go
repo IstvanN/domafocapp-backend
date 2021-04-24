eab4d664641ef2c7bf5396f30153751e6ffdd0ba
Istvan Nemeth <nemethistvan.ius@gmail.com>
2021-04-24 23:02:50 +0200

    feat(port): add dynamic port mapping

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/IstvanN/domafocapp-backend/controllers"
 	"github.com/IstvanN/domafocapp-backend/database"
 )
+
+var port = ":" + os.Getenv("PORT")
 
 func main() {
 	router := controllers.StartupRouter()
 
 	database.StartupPostgres()
 	log.Println("domafocapp-backend is up and running on port :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(port, router))
 }
